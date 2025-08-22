6ed098b132aa6ab69c1aa807e1a7f57ae8c20d5e
shreyastalole <shreyastalole@gmail.com>
2025-08-22 15:49:43 +0200

    Delete local builds

--- build/jetracer_description/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jetracer/ros2_ws/src/jetracer_description/install/jetracer_description'
